d097a58570b388e7c43cf62d52f8b27d0e02738e
Achilles <achipap@hotmail.com>
2020-03-21 18:20:14 +0100

    Update application.py

--- application.py
+++ application.py
@@ -10,10 +10,6 @@ import json
 
 greece = {"country":"Greece","total_cases":"0","active_cases":"0","new_cases":"0","total_recovered":"0","serious_cases":"0","total_deaths":"0","new_deaths":"0","total_cases_per_mil":"0"}
 
-if __name__ == '__main__':
-    run_check()
-
-
 # Send message with telegram bot
 def bot_send_message(message):
     bot_link = "https://api.telegram.org/bot1141616070:AAHfA9xi3qe067xL4qBUUWfnn7x_ln6-rA8/sendMessage?chat_id=@coronavirusingreece&text="
@@ -39,7 +35,76 @@ def run_check():
         total_cases_per_mil = json_res["total_cases_per_mil"]
         
         if(sorted(greece.items()) != sorted(json_res.items())):
-
+            
+            
+            total_cases_string = "%0AΣυνολικά κρούσματα: "
+            if(greece['total_cases'] != json_res['total_cases'] && json_res['total_cases'] != "No Data"):
+                total_cases_string += json_res['total_cases'].encode("utf-8")
+                total_cases_string += " [NEO]"
+            else:
+                total_cases_string += greece['total_cases'].encode("utf-8")
+                
+            active_cases_string = "%0AΕνεργά κρούσματα: "
+            if(greece['active_cases'] != json_res['active_cases'] && json_res['active_cases'] != "No Data"):
+                active_cases_string += json_res['active_cases'].encode("utf-8")
+                active_cases_string += " [NEO]"
+            else:
+                active_cases_string = greece['active_cases'].encode("utf-8")
+                
+            new_cases_string = "%0AΝέα κρούσματα: "
+            if(greece['new_cases'] != json_res['new_cases'] && json_res['new_cases'] != "No Data"):
+                new_cases_string += json_res['new_cases'].encode("utf-8")
+                new_cases_string += " [NEO]"
+            else:
+                new_cases_string = greece['new_cases'].encode("utf-8")
+                
+            total_recovered_string = "%0AΈχουν αναρρώσει: "
+            if(greece['total_recovered'] != json_res['total_recovered'] && json_res['total_recovered'] != "No Data"):
+                total_recovered_string += json_res['total_recovered'].encode("utf-8")
+                total_recovered_string += " [NEO]"
+            else:
+                total_recovered_string = greece['total_recovered'].encode("utf-8")
+                
+            serious_cases_string = "%0AΣοβαρά περιστατικά: "
+            if(greece['serious_cases'] != json_res['serious_cases'] && json_res['serious_cases'] != "No Data"):
+                serious_cases_string += json_res['serious_cases'].encode("utf-8")
+                serious_cases_string += " [NEO]"
+            else:
+                serious_cases_string = greece['serious_cases'].encode("utf-8")
+                
+            total_deaths_string = "%0AΣυνολικοί θάνατοι: "
+            if(greece['total_deaths'] != json_res['total_deaths'] && json_res['total_deaths'] != "No Data"):
+                total_deaths_string += json_res['total_deaths'].encode("utf-8")
+                total_deaths_string += " [NEO]"
+            else:
+                total_deaths_string = greece['total_deaths'].encode("utf-8")
+                
+            new_deaths_string = "%0AΝέοι θάνατοι: "
+            if(greece['new_deaths'] != json_res['new_deaths'] && json_res['new_deaths'] != "No Data"):
+                new_deaths_string += json_res['new_deaths'].encode("utf-8")
+                new_deaths_string += " [NEO]"
+            else:
+                new_deaths_string = greece['new_deaths'].encode("utf-8")
+                
+            new_deaths_string = "%0AΚρούσματα ανά 1.000.000 πλυθησμού: "
+            if(greece['total_cases_per_mil'] != json_res['total_cases_per_mil'] && json_res['total_cases_per_mil'] != "No Data"):
+                total_cases_per_mil_string += json_res['total_cases_per_mil'].encode("utf-8")
+                total_cases_per_mil_string += " [NEO]"
+            else:
+                total_cases_per_mil_string = greece['total_cases_per_mil'].encode("utf-8")
+                
+            string = "Έκτακτη Ενημέρωση"
+            string += total_cases_string
+            string += active_cases_string
+            string += new_cases_string
+            string += total_recovered_string
+            string += serious_cases_string
+            string += total_deaths_string
+            string += new_deaths_string
+            string += total_cases_per_mil_string
+            print(string)
+            bot_send_message(string)
+            
             greece['total_cases'] = json_res['total_cases']
             greece['active_cases'] = json_res['active_cases']
             greece['new_cases'] = json_res['new_cases']
@@ -48,25 +113,11 @@ def run_check():
             greece['total_deaths'] = json_res['total_deaths']
             greece['new_deaths'] = json_res['new_deaths']
             greece['total_cases_per_mil'] = json_res['total_cases_per_mil']
-            string = "Συνολικά κρούσματα: "
-            string += greece['total_cases'].encode("utf-8")
-            string += "%0AΕνεργά κρούσματα: "
-            string += greece['active_cases'].encode("utf-8")
-            string += "%0AΝέα κρούσματα: "
-            string += greece['new_cases'].encode("utf-8")
-            string += "%0AΈχουν αναρρώσει: "
-            string += greece['total_recovered'].encode("utf-8")
-            string += "%0AΣοβαρά περιστατικά: "
-            string += greece['serious_cases'].encode("utf-8")
-            string += "%0AΣυνολικοί θάνατοι: "
-            string += greece['total_deaths'].encode("utf-8")
-            string += "%0AΝέοι θάνατοι: "
-            string += greece['new_deaths'].encode("utf-8")
-            string += "%0AΚρούσματα ανά 1.000.000 πλυθησμού: "
-            string += greece['total_cases_per_mil'].encode("utf-8")
-            print(string)
-            bot_send_message(string)
         
     else:
         return None
     
+    
+if __name__ == '__main__':
+    run_check()
+
